30e1e11d46f9aa5758f1ce6e8787639de61b14a4
pthomson <patrick@Patricks-MacBook-Pro.local>
2018-04-26 14:30:41 -0400

    variables for repo

--- makeMe.sh
+++ makeMe.sh
@@ -2,9 +2,11 @@
 
 images="100"
 count="0"
+repo="testRepo"
+image="alpine-mass"
 
-while x [ $images -lt $count]
+while [ ${images} -gt ${count} ];
 do
-	docker build -t repo/image:$count .
+        docker build -t $repo/$image:$count .
 count =$[$count+1]
 done
