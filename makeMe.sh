867e85971bdd5e9ce73e52c2172a72dc555d0a8b
pthomson <patrick@Patricks-MacBook-Pro.local>
2018-04-26 15:00:03 -0400

    adding the option for removal

--- makeMe.sh
+++ makeMe.sh
@@ -1,12 +1,22 @@
 #!/bin/bash
 
+#editor: cpt_redbeard
+#Task: build a ton of images to test with
+
+#modifiable values
 images="100"
-count="0"
-repo="testRepo"
+repo="imagerepo"
 image="alpine-mass"
+
+#constant
+count="0"
+
+
+if 
 
 while [ ${images} -gt ${count} ];
 do
+        #docker rmi $repo/$image:$count
         docker build -t $repo/$image:$count .
-count =$[$count+1]
+count=$[$count+1]
 done
