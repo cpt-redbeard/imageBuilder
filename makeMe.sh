dcdbb7ef78d132151d1f1c8504e4e60541bf31c4
pthomson <patrick@Patricks-MacBook-Pro.local>
2018-05-09 01:25:10 -0400

    add option for retagging and pushing

--- makeMe.sh
+++ makeMe.sh
@@ -4,14 +4,18 @@
 #Task: build a ton of images to test with
 
 #modifiable values
-images="100"
+images="10000"
 repo="imagerepo"
 image="alpine-mass"
+registry="NewRegistryName"
+newrepo="NewRepoName"
+newimage="NewImageName"
 
 #constant
 count="0"
 
-echo "Make or Destroy?"
+echo "Make, Tag, Push or Destroy?"
+echo "Push only pushes from the newtag"
 read decision
 
 case $decision in
@@ -22,6 +26,28 @@ while [ ${images} -gt ${count} ];
 do
         docker rmi $repo/$image:$count
         #docker build -t $repo/$image:$count .
+count=$[$count+1]
+done
+;;
+
+[Tt] | [Tt][Aa][Gg] )
+echo "Tagging..."
+
+while [ ${images} -gt ${count} ];
+do
+        #docker rmi $repo/$image:$count
+        docker tag $repo/$image:$count $registry/$newrepo/$newimage:$count
+count=$[$count+1]
+done
+;;
+
+[Pp] | [Pp][Uu][Ss][Hh] )
+echo "Pushing..."
+
+while [ ${images} -gt ${count} ];
+do
+        #docker rmi $repo/$image:$count
+        docker push $registry/$newrepo/$newimage:$count
 count=$[$count+1]
 done
 ;;
@@ -38,7 +64,7 @@ done
 ;;
 
 *)
-	echo "Unkown choice, please choose Make or Destroy"
+	echo "Unkown choice, please select one of the above"
 	;;
 
 esac
